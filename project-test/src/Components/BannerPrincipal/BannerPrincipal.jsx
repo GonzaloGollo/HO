9c5f8fc35ff43d87bb483680830b951ba6f9eb44
GonzaloGollo <gonzalogollo@gmail.com>
2023-08-17 11:28:16 -0300

    Responsive al banner principal, modificacion sombras y tipos a los textos del banner y proporciones

--- project-test/src/Components/BannerPrincipal/BannerPrincipal.jsx
+++ project-test/src/Components/BannerPrincipal/BannerPrincipal.jsx
@@ -1,33 +1,26 @@
-import React from 'react'
-import "./BannerPrincipal.css"
+import React from "react";
+import "./BannerPrincipal.css";
 
 const BannerPrincipal = () => {
   return (
     <div>
-    <div>BannerPrincial</div>
-        <div className='flexing'>
-            <div className='section section--yellow'>
-                <h3 className='title'>Co Working</h3>
-                <h3 className='precio'>$2.500</h3>
-
-            </div>
-            <div className='section section--pink'>
-                <h3 className='title'>Office house</h3>
-                <h3 className='precio'>$3.500</h3>
-            </div>
-            <div className='section section--green'>
-                <h3 className='title'>Oficina Privada</h3>
-                <h3 className='precio'>$4.500</h3>
-
-            </div>
-
+      <div>BannerPrincial</div>
+      <div className="flexing">
+        <div className="section section--yellow">
+          <h3 className="title">Co Working</h3>
+          <h3 className="precio">$2.500</h3>
         </div>
-
-
-
-
+        <div className="section section--pink">
+          <h3 className="title">Office house</h3>
+          <h3 className="precio">$3.500</h3>
+        </div>
+        <div className="section section--green">
+          <h3 className="title">Oficina Privada</h3>
+          <h3 className="precio">$4.500</h3>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BannerPrincipal
+export default BannerPrincipal;
